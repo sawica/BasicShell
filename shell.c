50b9e865fd0e94b696d2b3de8f2680de4fe68756
Sawi <61832069+sawica@users.noreply.github.com>
2021-04-24 17:23:38 +0200

    Add files via upload

--- shell.c
+++ shell.c
@@ -7,39 +7,78 @@
 
 #define clear() printf("\033[H\033[J")
 
-void introduction(char* user){
 
-    // clear();
-    char* hello="***Hello ";
-    char* hello2 = " !***\n Welcome to our BasicShell\n";
-   
+char* mread(char* ustring) {
+     char buf[2] = { 0, 0 };
+    while (buf[0] != '\n') {
+      read(STDIN_FILENO, buf, 1);
+
+      if (buf[0] != '\n') {
+      	strcat(ustring, buf);
+      }
+    }
+    return ustring;
+}
+
+void introduction(char* user) {
+
+    clear();
+    char* hello="Hello ";
+    char* hello2 = " ! Welcome to our BasicShell\n\n";
+
     write(STDOUT_FILENO, hello, strlen(hello));
     write(STDOUT_FILENO, user, strlen(user));
     write(STDOUT_FILENO, hello2, strlen(hello2));
-    // fflush(stdout);
-    
-    /*fwrite(hello, sizeof(hello), sizeof(char), *stdout);
-    fwrite(user, sizeof(user), sizeof(char), stdout);
-    fwrite(hello2, sizeof(hello2), sizeof(char), stdout);
-    *///write(STDOUT_FILENO,"***Hello %s !***\n Welcome to our BasicShell\n", user, sizeof(user+42));
-    printf("If you need more informations about how to use this shell just write 'help' in command line. \n");
-    sleep(5);
-    // clear();
+
+    char* help = "*If you need more informations about how to use this shell just write 'help' in command line. \n";
+    write(STDOUT_FILENO, help, strlen(help));
+    sleep(3);
+     clear();
 }
 
 void showUserName_and_Dir(char* user){
-    char* dir;
-    getcwd(dir, sizeof(dir));
-  //  write(STDOUT_FILENO,"\n %s :~ %s", user, dir, 7+sizeof(user)+sizeof(dir));
+    char* dir= malloc(100 * sizeof(char));
+    getcwd(dir, 100*sizeof(char));
+
+    char* space= " :~ ";
+    write(STDOUT_FILENO, user, strlen(user));
+    write(STDOUT_FILENO, space, strlen(space));
+    write(STDOUT_FILENO, dir, strlen(dir));
+
+
 }
 
 char* readLine(){
 
+    size_t line_size = 1000;
+    char* line = malloc(line_size * sizeof(char));
+    size_t temp = getline(&line, &line_size, stdin);
+    // zrbic obsluge bledu 
+    write(STDOUT_FILENO,line, strlen(line));
 
-    // return line;
+    return line;
 }
 
+
 char** tokenize(char** old_tokens, char delimeter) {
+    char** token=malloc(50*sizeof(char));
+    for(int i=0; i<50;i++){
+        token[i]=malloc(20*sizeof(char));
+    }
+    char buf[2] = { 0, 0 };
+    int tmp=0;
+    for(int j=0; j<50; j++){    
+        if(buf == NULL) j++;
+        read(old_tokens, buf, 1);
+        if (buf[0] != delimeter) {
+      	strcat(token[tmp], buf);
+        }
+        else {
+            strcat(token[tmp++], buf);
+            tmp++;
+        }
+    }
+    return token;
 	// delimeter = ' ', old_tokens = ["ls -1 | grep .c|xargs wc -l|sort >>file.txt"]
 	// return ["ls", " ", "-1", " ", "|", " ", "grep", " ", ".c|xargs", " ", "wc",  " ","-l|sort", " ", ">>file.txt"]
 	//
@@ -59,12 +98,14 @@ int parse_commands(char* line, char** commands){
     }
 }
 
-void handle_commands(char* user, char** first_command, char** second_command){
+void handle_commands(char* user//, char** first_command, char** second_command
+){
     //tu deklaracje
     char* line;
     char* commands[4];
     int one_or_two = 0;
     int sum=0;
+    showUserName_and_Dir(user);
     while(1){
         showUserName_and_Dir(user);
         line = readLine();
@@ -98,17 +139,7 @@ int main(){
     char* user = malloc(16 * sizeof(char));
 
     write(STDOUT_FILENO, "Your user name: ", 15);
-
-
-    char buf[2] = { 0, 0 };
-    while (buf[0] != '\n') {
-      read(STDIN_FILENO, buf, 1);
-
-      if (buf[0] != '\n') {
-      	strcat(user, buf);
-      }
-    }
-    // fscanf(stdin, "%s", user);
+    mread(user);
     introduction(user);
-   // handle_commands(user);
+    handle_commands(user);
 }
